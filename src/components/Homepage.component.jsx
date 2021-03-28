e9b4b9f7e2750ae0e84bb1618e1dbf2777020d52
John Elesho <eleshojohno@gmail.com>
2021-03-28 14:05:04 +0100

    Created project and Added an Homepage

---
+++ src/components/Homepage.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                                Product
+                        </h1>
+                        <span className="subtitle">
+                                Grace
+                        </span>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Homepage
